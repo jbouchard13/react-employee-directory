81fae98ac74d95d5eb3bd15dfcb90f6402db2287
John Bouchard <jbouchard13@gmail.com>
2020-08-13 15:36:00 -0400

    updated for heroku deployment

---
+++ client/build/precache-manifest.d04655bf1a7da841d142ed35bb2a411c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "29d6ddae0d5286af1dbb",
+    "url": "/static/css/main.bd747fd0.chunk.css"
+  },
+  {
+    "revision": "29d6ddae0d5286af1dbb",
+    "url": "/static/js/main.64280ba6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "fc4b78e877d592e59a26",
+    "url": "/static/css/2.7771aa4b.chunk.css"
+  },
+  {
+    "revision": "fc4b78e877d592e59a26",
+    "url": "/static/js/2.9a0d1534.chunk.js"
+  },
+  {
+    "revision": "1ee7911acf57d5da2cbdbac161cfa7f5",
+    "url": "/index.html"
+  }
+];
--- server.js
+++ server.js
@@ -8,6 +8,10 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+}
+
 app.use(apiRoutes);
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/employee', {
