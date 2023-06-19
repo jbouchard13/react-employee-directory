869650b0a913b62e3743f561e27ff0bfda7cd02c
John Bouchard <jbouchard13@gmail.com>
2023-06-18 21:57:07 -0400

    worked on button styling

--- client/src/components/TableRow/index.js
+++ client/src/components/TableRow/index.js
@@ -1,19 +1,22 @@
 import React from "react";
 import Button from "../Button";
+import "./style.css";
 
 class TableRow extends React.Component {
   render() {
     return (
       <tr key={this.props.key}>
         <td>
-          <Button
-            buttonText="X"
-            className="delete-btn"
-            handleSort={() => {
-              this.props.deleteEmployee(this.props.id);
-            }}
-          />
-          {this.props.firstName}
+          <div className="del-btn-wrapper">
+            <Button
+              buttonText="X"
+              className="delete-btn"
+              handleSort={() => {
+                this.props.deleteEmployee(this.props.id);
+              }}
+            />
+            {this.props.firstName}
+          </div>
         </td>
         <td>{this.props.lastName}</td>
         <td>{this.props.jobTitle}</td>
